988e97d2bf69a867e1eb2580f932c6ead0d8da28
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-30 00:00:13 -0300

    faz a 7 e corrige a 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-CREATE faturamento_atual AS 
+CREATE VIEW faturamento_atual AS 
 SELECT MIN(account_price) AS "faturamento_minimo", MAX(account_price) AS "faturamento_maximo", CAST(AVG(account_price) AS DECIMAL(10,2)) AS  "faturamento_medio", SUM(account_price) AS "faturamento_total"
 FROM user_profile as t1
 INNER JOIN user_plans as t2
---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+CREATE VIEW perfil_artistas AS 
+SELECT artist_name AS 'artista', album_name AS 'album', COUNT(t3.user_id) AS 'seguidores' from albuns as t1
+INNER JOIN artists as t2
+ON t2.artist_id = t1.artist_id
+INNER JOIN user_following as t3
+ON t3.artist_id = t1.artist_id
+GROUP BY album_name, artist_name
+ORDER BY COUNT(t3.user_id) DESC, artist_name, album_name;
