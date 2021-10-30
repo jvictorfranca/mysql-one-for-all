af48965c32cc7f2501927cb7b87482bc16063083
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-30 00:30:05 -0300

    corrige 5 e 6

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,5 @@ SELECT song_name AS 'cancao', COUNT(*) as "reproducoes" from user_songs_history 
 INNER JOIN songs AS t2
 ON t2.song_id = t1.song_id
 GROUP BY song_name
-ORDER BY COUNT(0) DESC
+ORDER BY COUNT(0) DESC, cancao
 LIMIT 2;
