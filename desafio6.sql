fa89fd8b88567e8bd91e378e14982a62502d642d
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-29 23:43:04 -0300

    faz o 6

---
+++ desafio6.sql
@@ -0,0 +1,5 @@
+CREATE faturamento_atual AS 
+SELECT MIN(account_price) AS "faturamento_minimo", MAX(account_price) AS "faturamento_maximo", CAST(AVG(account_price) AS DECIMAL(10,2)) AS  "faturamento_medio", SUM(account_price) AS "faturamento_total"
+FROM user_profile as t1
+INNER JOIN user_plans as t2
+ON t1.plan_id = t2.plan_id;
