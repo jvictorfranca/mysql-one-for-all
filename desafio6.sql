9c3e854b0a65ce46c549e49726e27e880ca15824
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-30 00:51:05 -0300

    agora vai o 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 CREATE VIEW faturamento_atual AS 
-SELECT CONVERT(MIN(account_price), CHAR(10)) AS "faturamento_minimo", CONVERT(MAX(account_price),CHAR(10)) AS "faturamento_maximo", CONVERT(CAST(AVG(account_price) AS DECIMAL(10,2)),CHAR(10)) AS  "faturamento_medio", SUM(account_price) AS "faturamento_total"
+SELECT CONVERT(CAST(MIN(account_price) AS DECIMAL(10,2)), CHAR(10)) AS "faturamento_minimo", CONVERT(MAX(account_price),CHAR(10)) AS "faturamento_maximo", CONVERT(CAST(AVG(account_price) AS DECIMAL(10,2)),CHAR(10)) AS  "faturamento_medio", CONVERT(CAST(SUM(account_price) AS DECIMAL(10,2)), CHAR(10)) AS "faturamento_total"
 FROM user_profile as t1
 INNER JOIN user_plans as t2
 ON t1.plan_id = t2.plan_id;
