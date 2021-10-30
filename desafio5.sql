720edf16ca7f9ced136dec4233fde0e94345bdbd
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-29 23:46:47 -0300

    corrige o 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 CREATE VIEW top_2_hits_do_momento AS 
-ELECT song_name AS 'cancao', COUNT(*) as "reproducoes" from user_songs_history AS t1
+SELECT song_name AS 'cancao', COUNT(*) as "reproducoes" from user_songs_history AS t1
 INNER JOIN songs AS t2
 ON t2.song_id = t1.song_id
 GROUP BY song_name
