3e8efad4a7f88c80d60f4e6694e12191988915d5
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-31 22:11:59 -0300

    tenta o 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON user_profile
+    FOR EACH ROW
+BEGIN
+    DELETE FROM user_following
+    WHERE user_id = old.user_id;
+    
+    DELETE FROM user_songs_history
+    WHERE user_id = OLD.user_id;
+END $$
+DELIMITER ;
