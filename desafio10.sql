b49d233bac44549fbf3294543926666a9036693e
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-31 23:07:53 -0300

    tenta arrumar o 10 corrigindo o name com o ID porque o teste esta aparentemente errado

--- desafio10.sql
+++ desafio10.sql
@@ -2,14 +2,14 @@ USE SpotifyClone;
 
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(username VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(userid INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE songs_total INT;
  SELECT COUNT(*) FROM user_songs_history as t1
 	INNER JOIN user_profile as t2
 	ON t1.user_id = t2.user_id
-	WHERE t2.user_name = username
+	WHERE t2.user_id = userid
     INTO songs_total;
     RETURN songs_total;
 END $$
